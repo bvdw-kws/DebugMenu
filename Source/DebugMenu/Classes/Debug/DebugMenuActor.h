31e57fe76e919bb11934fb4d11951b45b1f99d47
BastienVdW <82994796+BastienVdW@users.noreply.github.com>
2026-07-10 12:12:42 +0900

    Update copyright notices to comply with Apache License 2.0

--- Source/DebugMenu/Classes/Debug/DebugMenuActor.h
+++ Source/DebugMenu/Classes/Debug/DebugMenuActor.h
@@ -1,9 +1,8 @@
 // Copyright (C) 2024 Van de Walle Bastien
-// All Rights Reserved.
-//
-// This software is licensed for use in Unreal Engine projects only.
-// Redistribution or sharing of source code is prohibited.
-// See LICENSE file for complete terms.
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// http://www.apache.org/licenses/LICENSE-2.0
 
 #pragma once
 
